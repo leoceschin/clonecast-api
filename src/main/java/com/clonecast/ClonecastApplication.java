c5fd3c6c5aaa44198e66b321690302377fb49fd2
leona <leonardofcoceschin@gmail.com>
2022-12-08 08:57:59 -0300

    projeto criado

---
+++ src/main/java/com/clonecast/ClonecastApplication.java
@@ -0,0 +1,13 @@
+package com.clonecast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClonecastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClonecastApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clonecast/ClonecastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clonecast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClonecastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
